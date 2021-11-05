dfd7216482027f9eb85f88b54001f82661296473
AndresArana <ragaandres1124@gmail.com>
2021-11-05 12:12:40 -0500

    second commit

--- backend/controllers/role.js
+++ backend/controllers/role.js
@@ -1,5 +1,5 @@
 import role from "../models/role.js";
-
+//validar si existe los datos
 const registerRole = async (req, res) => {
   if (!req.body.name || !req.body.description)
     return res.status(400).send("incomplete data");
@@ -12,8 +12,17 @@ const registerRole = async (req, res) => {
     description: req.body.description,
     dbStatus: true,
   });
+  //guardar datos -> post
   const result = await roleSchema.save();
   if (!result) return res.status(400).send("failed to register role");
   return res.status(200).send({ result });
 };
-export default {registerRole};
+//listar roles -> get
+const listRole = async (req,res)=>{
+  const roleSchema = await role.find();
+  if(!roleSchema || roleSchema.length == 0) return res.status(400).send("Empty role list");
+  return res.status(200).send({roleSchema})
+  //return !roleSchema || roleSchema.length ==0 ?
+}
+export default {registerRole, listRole};
+
